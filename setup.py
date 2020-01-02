2a04d608fbfa93380b0ef0d97cb59c6efb3ca871
Henry Xu <henry.xu@ringcentral.com>
2020-01-02 10:45:12 +0800

    chore(*): initialize

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyautogui-ext",
+    version="0.0.1",
+    author="Weihong Xu",
+    author_email="xuweihong.cn@gmail.com",
+    description="helpful extension for pyautogui",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/link89/pyautogui-ext",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'fire',
+        'opencv-python-headless == 3.4.2.16',
+        'opencv-contrib-python-headless == 3.4.2.16',
+        'numpy',
+        'matplotlib',
+    ],
+)
