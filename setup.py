987a56741459126e0dac5e90a7a0e1cef76e2d1e
Henry Xu <henry.xu@ringcentral.com>
2020-01-09 09:32:51 +0800

    wip: using feature matching for locate on screen

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=[
         'fire',
+        'pyscreeze',
         'opencv-python-headless == 3.4.2.16',
         'opencv-contrib-python-headless == 3.4.2.16',
         'numpy',
